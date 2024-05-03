5d2a8ad57e53c7cadef2d68584b007bcfbde2a7e
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-05-04 00:32:47 +0530

    Template literal - ``

--- 04_01/script.js
+++ 04_01/script.js
@@ -6,7 +6,7 @@
 import Backpack from "./Backpack.js";
 
 const everydayPack = new Backpack(
-  "Everyday Backpack",
+  "Green Frog",
   30,
   "grey",
   15,
@@ -15,6 +15,25 @@ const everydayPack = new Backpack(
   false,
   "December 5, 2018 15:00:00 PST"
 );
+
+const content = `
+        <main>
+          <article>
+            <h1>${everydayPack.name}</h1>
+            <ul>
+              <li>Volume:${everydayPack.volume}</li>
+              <li>Color:${everydayPack.color}</li>
+              <li>Age:${everydayPack.backpackAge()}</li>
+              <li>Number of pockets:${everydayPack.pocketNum}</li>
+              <li>Left strap length:${everydayPack.strapLength.left}</li>
+              <li>Right strap length:${everydayPack.strapLength.right}</li>
+              <li>Lid status:${everydayPack.lidOpen}</li>
+            </ul>
+          </article>
+        </main>
+`;
+
+document.body.innerHTML = content;
 
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);

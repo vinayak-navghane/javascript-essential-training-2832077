1f0af627af49d9ab931b3424994d4640c45f4ffc
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-05-14 21:29:54 +0530

    function practice

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,25 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+const groceriesList = {
+  shopName: "Dmart",
+  item1: "Milk",
+  item2: "Coffee",
+  item3: "Suger",
+};
+
+function firstFunction(shoppingList) {
+  const newArticle = document.createElement("shoppingDetails");
+  newArticle.innerHTML = `
+    <h1>Shopping from ${shoppingList.shopName}</h1>
+    <ul>
+       <ol>1.${shoppingList.item1}</ol>
+       <ol>2.${shoppingList.item2}</ol>
+       <ol>3.${shoppingList.item3}</ol>
+    </ul>`;
+  return newArticle;
+}
+
+const main = document.querySelector("main");
+main.append(firstFunction(groceriesList));
